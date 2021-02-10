f4ab699b256249d9fbb04fc6d8ec933efdf3cc2c
yumekiti <yumekiti120440@gmail.com>
2021-02-11 07:00:41 +0900

    リンクを変えた

---
+++ vue/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Home">
+    <router-link to='/hello'>HelloWorld</router-link>
+    <p>{{ hoge }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data: function() {
+    return {
+     hoge: 'test'
+    }
+  }
+}
+</script>
+
