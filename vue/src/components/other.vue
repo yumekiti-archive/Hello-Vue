70f0a8245642ff621b16c5545faa9ea4a83a7596
yumekiti <yumekiti120440@gmail.com>
2021-02-20 17:26:16 +0000

    add:other-2

--- vue/src/components/other.vue
+++ vue/src/components/other.vue
@@ -7,6 +7,12 @@
 
     <div id="other-1">
       <button v-on:click.once="myClick">click me!</button>
+    </div><br><hr>
+
+    <div id="other-2">
+      <p>内容：{{ naiyou }}</p>
+      <p>選択：{{ sel }}</p>
+      <textarea rows="4" cols="40" v-model="naiyou" @select="selected"></textarea>
     </div>
 
   </div>
@@ -15,9 +21,18 @@
 <script>
 export default {
   name: 'other',
+  data() {
+    return{
+      sel:"",
+      naiyou:""
+    }
+  },
   methods: {
     myClick() {
       alert("click!!")
+    },
+    selected: function() {
+      this.sel = window.getSelection().toString();
     }
   }
 }
